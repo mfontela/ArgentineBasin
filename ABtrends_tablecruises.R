bb66f3f21b8924cd1f53c0c0c95a5e4f2ea1fe7e
mfontela <mmfontela@ualg.pt>
2020-11-11 12:27:05 +0000

    Update ABtrends_tablecruises.R

--- ABtrends_tablecruises.R
+++ ABtrends_tablecruises.R
@@ -11,11 +11,3 @@ tablecruises<-all%>%
   select(-EXPOCODE, -expocodeno)%>%
   rename(Expocode=expocode)%>%
   mutate(Source=ifelse(Year<1990 & Year>1987, "CCHDO", "GLODAPv2.2020"))
-
-library("xlsx")
-if (export_to_excel) {
-  write.xlsx(tablecruises,"C:/Users/MFontela/OneDrive - Universidade do Algarve/ABtrends/DATA/ABtrends_Tablecruises.xlsx",
-             sheetName = "tablecruises", 
-             col.names = TRUE, row.names = FALSE)
-  shell.exec("C:/Users/MFontela/OneDrive - Universidade do Algarve/ABtrends/DATA/ABtrends_Tablecruises.xlsx")
-  }
